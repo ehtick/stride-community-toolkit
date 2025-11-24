5e7240e6404fb1b128a504c6ff611a6c333b1925
Vaclav Elias <4528464+VaclavElias@users.noreply.github.com>
2025-11-24 14:10:05 +0000

    feat: Serilog logging added

--- tools/Stride.CommunityToolkit.Examples.MetadataGenerator/Program.cs
+++ tools/Stride.CommunityToolkit.Examples.MetadataGenerator/Program.cs
@@ -1,11 +1,19 @@
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Serilog;
 using Stride.CommunityToolkit.Examples.MetadataGenerator;
 using Stride.CommunityToolkit.Examples.MetadataGenerator.Commands;
 using Stride.CommunityToolkit.Examples.MetadataGenerator.Services;
 
-// Build the dependency injection container
 var builder = Host.CreateApplicationBuilder(args);
+
+builder.Services.AddSerilog((services, configuration) =>
+{
+    configuration
+        .ReadFrom.Configuration(builder.Configuration)
+        .ReadFrom.Services(services)
+        .Enrich.FromLogContext();
+});
 
 builder.Services.AddScoped<ManifestService>();
 builder.Services.AddScoped<ScanCommandHandler>();
@@ -13,9 +21,8 @@ builder.Services.AddScoped<GenerateCommandHandler>();
 
 using var host = builder.Build();
 
-// Create and configure the CLI
 var cliConfiguration = new CommandLineConfiguration(host.Services);
 var rootCommand = cliConfiguration.CreateRootCommand();
-
 var parseResult = rootCommand.Parse(args);
+
 return parseResult.Invoke();
