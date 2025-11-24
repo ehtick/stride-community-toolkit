3625ad8287cbfbe5057d3252ffbf61eeb899c462
Vaclav Elias <4528464+VaclavElias@users.noreply.github.com>
2025-11-24 13:46:23 +0000

    Integrate Serilog with Host.CreateApplicationBuilder

--- tools/Stride.CommunityToolkit.Examples.MetadataGenerator/Program.cs
+++ tools/Stride.CommunityToolkit.Examples.MetadataGenerator/Program.cs
@@ -1,21 +1,50 @@
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Serilog;
 using Stride.CommunityToolkit.Examples.MetadataGenerator;
 using Stride.CommunityToolkit.Examples.MetadataGenerator.Commands;
 using Stride.CommunityToolkit.Examples.MetadataGenerator.Services;
 
-// Build the dependency injection container
-var builder = Host.CreateApplicationBuilder(args);
+// Configure Serilog early (before creating the host)
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .CreateBootstrapLogger();
 
-builder.Services.AddScoped<ManifestService>();
-builder.Services.AddScoped<ScanCommandHandler>();
-builder.Services.AddScoped<GenerateCommandHandler>();
+try
+{
+    Log.Information("Starting Stride Examples Metadata Generator");
 
-using var host = builder.Build();
+    // Build the dependency injection container
+    var builder = Host.CreateApplicationBuilder(args);
 
-// Create and configure the CLI
-var cliConfiguration = new CommandLineConfiguration(host.Services);
-var rootCommand = cliConfiguration.CreateRootCommand();
+    // Configure Serilog from appsettings.json
+    builder.Services.AddSerilog((services, loggerConfiguration) =>
+    {
+        loggerConfiguration
+            .ReadFrom.Configuration(builder.Configuration)
+            .ReadFrom.Services(services);
+    });
 
-var parseResult = rootCommand.Parse(args);
-return parseResult.Invoke();
+    builder.Services.AddScoped<ManifestService>();
+    builder.Services.AddScoped<ScanCommandHandler>();
+    builder.Services.AddScoped<GenerateCommandHandler>();
+
+    using var host = builder.Build();
+
+    // Create and configure the CLI
+    var cliConfiguration = new CommandLineConfiguration(host.Services);
+    var rootCommand = cliConfiguration.CreateRootCommand();
+
+    // Parse and invoke
+    var parseResult = rootCommand.Parse(args);
+    return parseResult.Invoke();
+}
+catch (Exception ex)
+{
+    Log.Fatal(ex, "Application terminated unexpectedly");
+    return 1;
+}
+finally
+{
+    await Log.CloseAndFlushAsync();
+}
