da6cf343b3bcc8ca44e61ff4c62610f027bbdc13
Vaclav Elias <4528464+VaclavElias@users.noreply.github.com>
2025-11-30 20:22:05 +0000

    refactor: Code position updated

--- src/Stride.CommunityToolkit.Bullet/EntityExtensions.cs
+++ src/Stride.CommunityToolkit.Bullet/EntityExtensions.cs
@@ -11,41 +11,14 @@ namespace Stride.CommunityToolkit.Bullet;
 public static class EntityExtensions
 {
     /// <summary>
-    /// Adds Bullet 3D physics components to the entity.
+    /// Adds Bullet 2D physics components to the entity with an appropriate collider shape based on the primitive type.
     /// </summary>
-    public static Entity AddBullet3DPhysics(this Entity entity, PrimitiveModelType type, Bullet3DPhysicsOptions? options = null)
-    {
-        ArgumentNullException.ThrowIfNull(entity);
-
-        options ??= new();
-
-        if (options.PhysicsComponent is null) return entity;
-
-        if (!options.IncludeCollider)
-        {
-            entity.Add(options.PhysicsComponent);
-
-            return entity;
-        }
-
-        var colliderShape = Get3DColliderShape(type, options.Size);
-
-        if (colliderShape is null) return entity;
-
-        options.PhysicsComponent.ColliderShapes.Add(colliderShape);
-
-        entity.Add(options.PhysicsComponent);
-
-        return entity;
-    }
-
-    /// <summary>
-    /// Adds Bullet 2D physics components to the entity.
-    /// </summary>
-    /// <param name="entity"></param>
-    /// <param name="type"></param>
-    /// <param name="options"></param>
-    /// <returns></returns>
+    /// <param name="entity">The entity to add physics components to.</param>
+    /// <param name="type">The type of 2D primitive shape for the collider.</param>
+    /// <param name="options">Optional physics configuration including the physics component, size, depth, and whether to include a collider. If null, default options are used.</param>
+    /// <returns>The entity with the Bullet 2D physics components added.</returns>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="entity"/> is null.</exception>
+    /// <exception cref="InvalidOperationException">Thrown when an unsupported <see cref="Primitive2DModelType"/> is specified.</exception>
     public static Entity AddBullet2DPhysics(this Entity entity, Primitive2DModelType type, Bullet2DPhysicsOptions? options = null)
     {
         ArgumentNullException.ThrowIfNull(entity);
@@ -90,10 +63,7 @@ public static class EntityExtensions
             //var collider = new ConvexHullColliderShape(points, uintIndices, Vector3.Zero);
             //var cs = new PhysicsColliderShape(descriptions);
 
-
-            List<IAssetColliderShapeDesc> descriptions = [];
-
-            descriptions.Add(collider);
+            List<IAssetColliderShapeDesc> descriptions = [collider];
 
             var colliderShapeAsset = new ColliderShapeAssetDesc
             {
@@ -108,8 +78,6 @@ public static class EntityExtensions
         {
             var colliderShape = Get2DColliderShape(type, options.Size, options.Depth);
 
-            if (colliderShape is null) return entity;
-
             options.PhysicsComponent.ColliderShapes.Add(colliderShape);
         }
 
@@ -118,23 +86,75 @@ public static class EntityExtensions
         return entity;
     }
 
-    private static IInlineColliderShapeDesc? Get2DColliderShape(Primitive2DModelType type, Vector2? size = null, float depth = 0)
+    /// <summary>
+    /// Adds Bullet 3D physics components to the entity with an appropriate collider shape based on the primitive type.
+    /// </summary>
+    /// <param name="entity">The entity to add physics components to.</param>
+    /// <param name="type">The type of 3D primitive shape for the collider.</param>
+    /// <param name="options">Optional physics configuration including the physics component, size, and whether to include a collider. If null, default options are used.</param>
+    /// <returns>The entity with the Bullet 3D physics components added.</returns>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="entity"/> is null.</exception>
+    /// <remarks>
+    /// Some primitive types (Torus, Teapot) do not have a collider shape and will be added without a collider even if <c>IncludeCollider</c> is <see langword="true"/>.
+    /// </remarks>
+    public static Entity AddBullet3DPhysics(this Entity entity, PrimitiveModelType type, Bullet3DPhysicsOptions? options = null)
+    {
+        ArgumentNullException.ThrowIfNull(entity);
+
+        options ??= new();
+
+        if (options.PhysicsComponent is null) return entity;
+
+        if (!options.IncludeCollider)
+        {
+            entity.Add(options.PhysicsComponent);
+
+            return entity;
+        }
+
+        var colliderShape = Get3DColliderShape(type, options.Size);
+
+        if (colliderShape is null) return entity;
+
+        options.PhysicsComponent.ColliderShapes.Add(colliderShape);
+
+        entity.Add(options.PhysicsComponent);
+
+        return entity;
+    }
+
+    /// <summary>
+    /// Creates a 2D collider shape based on the specified primitive type.
+    /// </summary>
+    /// <param name="type">The type of 2D primitive shape.</param>
+    /// <param name="size">Optional size for the collider. Interpretation varies by shape type.</param>
+    /// <param name="depth">The depth of the 2D shape in the Z axis.</param>
+    /// <returns>An <see cref="IInlineColliderShapeDesc"/> configured for the specified primitive type.</returns>
+    /// <exception cref="InvalidOperationException">Thrown when an unsupported <see cref="Primitive2DModelType"/> is specified.</exception>
+    private static IInlineColliderShapeDesc Get2DColliderShape(Primitive2DModelType type, Vector2? size = null, float depth = 0)
         => type switch
         {
             Primitive2DModelType.Rectangle => size is null ? new BoxColliderShapeDesc() { Is2D = true } : new() { Size = new(size.Value.X, size.Value.Y, 0), Is2D = true },
             Primitive2DModelType.Square => size is null ? new BoxColliderShapeDesc() { Is2D = true } : new() { Size = new(size.Value.X, size.Value.Y, 0), Is2D = true },
             Primitive2DModelType.Circle => size is null ? new SphereColliderShapeDesc() : new() { Radius = size.Value.X, Is2D = true },
-            Primitive2DModelType.Capsule => size is null ? new CapsuleColliderShapeDesc() : new() { Radius = size.Value.X, Is2D = true },
+            Primitive2DModelType.Capsule => size is null ? new CapsuleColliderShapeDesc() : new() { Radius = size.Value.X, Length = size.Value.Y - 2 * size.Value.X, Is2D = true },
             _ => throw new InvalidOperationException(),
         };
 
+    /// <summary>
+    /// Creates a 3D collider shape based on the specified primitive type.
+    /// </summary>
+    /// <param name="type">The type of 3D primitive shape.</param>
+    /// <param name="size">Optional size for the collider. Interpretation varies by shape type (e.g., radius, dimensions).</param>
+    /// <returns>An <see cref="IInlineColliderShapeDesc"/> configured for the specified primitive type, or <c>null</c> if the shape type does not support colliders (e.g., Torus, Teapot).</returns>
+    /// <exception cref="InvalidOperationException">Thrown when an unsupported <see cref="PrimitiveModelType"/> is specified.</exception>
     private static IInlineColliderShapeDesc? Get3DColliderShape(PrimitiveModelType type, Vector3? size = null)
         => type switch
         {
             PrimitiveModelType.Plane => size is null ? new BoxColliderShapeDesc() : new() { Size = new Vector3(size.Value.X, 0, size.Value.Y) },
             PrimitiveModelType.InfinitePlane => new StaticPlaneColliderShapeDesc(),
             PrimitiveModelType.Sphere => size is null ? new SphereColliderShapeDesc() : new() { Radius = size.Value.X },
-            PrimitiveModelType.Cube => size is null ? new BoxColliderShapeDesc() : new() { Size = size ?? Vector3.One },
+            PrimitiveModelType.Cube => size is null ? new BoxColliderShapeDesc() : new() { Size = (Vector3)size },
             PrimitiveModelType.Cylinder => size is null ? new CylinderColliderShapeDesc() : new() { Radius = size.Value.X, Height = size.Value.Y },
             PrimitiveModelType.Torus => null,
             PrimitiveModelType.Teapot => null,
