a9d7662413884c58371be2158caf3fe35d4de01c
Vaclav Elias <4528464+VaclavElias@users.noreply.github.com>
2025-11-30 22:37:29 +0000

    fix: Capsule added back

--- examples/code-only/Example01_Basic3DScene/Program.cs
+++ examples/code-only/Example01_Basic3DScene/Program.cs
@@ -14,7 +14,7 @@ void Start(Scene rootScene)
     game.SetupBase3DScene();
     game.AddSkybox();
 
-    var entity = game.Create3DPrimitive(PrimitiveModelType.TriangularPrism);
+    var entity = game.Create3DPrimitive(PrimitiveModelType.Capsule);
     entity.Transform.Position = new Vector3(0, 8, 0);
     entity.Scene = rootScene;
 }
