07daff042cf397ffd8cbed643735741ded917479
Vaclav Elias <4528464+VaclavElias@users.noreply.github.com>
2025-11-30 20:47:13 +0000

    refactor: Code simplification

--- examples/code-only/Example01_Basic3DScene_Primitives/Program.cs
+++ examples/code-only/Example01_Basic3DScene_Primitives/Program.cs
@@ -41,68 +41,51 @@ void Update(Scene scene, GameTime time)
 
 void Add3DPrimitives(Scene scene)
 {
-    var cube = game.Create3DPrimitive(PrimitiveModelType.Cube,
-        new() { Size = size1 });
-    cube.Transform.Position = new Vector3(-4f, 0.5f, 0);
-    cube.Add(new DebugRenderComponentScript());
-    cube.Add(new CollidableGizmoScript()
+    var primitives = new[]
     {
-        Color = new Color4(0.4f, 0.843f, 0, 0.9f),
-        Visible = false
-    });
-    cube.Name = EntityName;
-    cube.Scene = scene;
+        (Type: PrimitiveModelType.Cube, Position: new Vector3(-4f, 0.5f, 0), Size: size1, Rotation: null, AddDebug: true),
+        (Type: PrimitiveModelType.Cube, Position: new Vector3(-2.2f, 0.5f, -4f), Size: size2, Rotation: null, AddDebug: false),
+        (Type: PrimitiveModelType.Cone, Position: new Vector3(0, 2, 0), Size: new (0.5f, 3, 0), Rotation: null, AddDebug: false),
+        (Type: PrimitiveModelType.Capsule, Position: new Vector3(0.01f, 6, 0), Size: size2, Rotation: null, AddDebug: false),
+        (Type: PrimitiveModelType.Sphere, Position: new Vector3(0, 8, 0), Size: size2, Rotation: null, AddDebug: false),
+        (Type: PrimitiveModelType.Cylinder, Position: new Vector3(0.5f, 10, 0), Size: size2, Rotation: null, AddDebug: false),
+        (Type: PrimitiveModelType.Teapot, Position: new Vector3(3, 4f, 0), Size: null, Rotation: null, AddDebug: false)!,
+        (Type: PrimitiveModelType.Torus, Position: new Vector3(0, 12, 0), Size: null, Rotation: null, AddDebug: false)!,
+        (Type: PrimitiveModelType.TriangularPrism, Position: new Vector3(-8.0f, 2, -3.0f), Size: (Vector3?)null, Rotation: (Quaternion?)Quaternion.RotationY(75), AddDebug: false),
+        (Type: PrimitiveModelType.TriangularPrism, Position: new Vector3(-3.5f, 0.5f, 1), Size: null, Rotation: null, AddDebug: false)!,
+    };
 
-    var cuboid = game.Create3DPrimitive(PrimitiveModelType.Cube,
-    new() { Size = size2 });
-    cuboid.Transform.Position = new Vector3(-2.2f, 0.5f, -4f);
-    cuboid.Name = EntityName;
-    cuboid.Scene = scene;
+    foreach (var primitive in primitives)
+    {
+        var entity = CreatePrimitive(primitive.Type, primitive.Position, primitive.Size, primitive.Rotation);
 
-    var cone = game.Create3DPrimitive(PrimitiveModelType.Cone,
-        new() { Size = new(0.5f, 3, 0) });
-    cone.Transform.Position = new Vector3(0, 2, 0);
-    cone.Name = EntityName;
-    cone.Scene = scene;
+        if (primitive.AddDebug)
+            AddDebugComponents(entity);
 
-    var capsule = game.Create3DPrimitive(PrimitiveModelType.Capsule,
-        new() { Size = size2 });
-    capsule.Transform.Position = new Vector3(0.01f, 6, 0);
-    capsule.Name = EntityName;
-    capsule.Scene = scene;
+        entity.Scene = scene;
+    }
 
-    var sphere = game.Create3DPrimitive(PrimitiveModelType.Sphere,
-        new() { Size = size2 });
-    sphere.Transform.Position = new Vector3(0, 8, 0);
-    sphere.Name = EntityName;
-    sphere.Scene = scene;
+    Entity CreatePrimitive(PrimitiveModelType type, Vector3 position, Vector3? size, Quaternion? rotation)
+    {
+        var entity = game.Create3DPrimitive(type, size.HasValue ? new() { Size = size.Value } : null);
+        entity.Transform.Position = position;
 
-    var cylinder = game.Create3DPrimitive(PrimitiveModelType.Cylinder,
-        new() { Size = size2 });
-    cylinder.Transform.Position = new Vector3(0.5f, 10, 0);
-    cylinder.Name = EntityName;
-    cylinder.Scene = scene;
+        if (rotation.HasValue)
+            entity.Transform.Rotation = rotation.Value;
 
-    var teapot = game.Create3DPrimitive(PrimitiveModelType.Teapot);
-    teapot.Transform.Position = new Vector3(3, 4f, 0);
-    teapot.Name = EntityName;
-    teapot.Scene = scene;
+        entity.Name = EntityName;
+        return entity;
+    }
 
-    var torus = game.Create3DPrimitive(PrimitiveModelType.Torus);
-    torus.Transform.Position = new Vector3(0, 12, 0);
-    torus.Name = EntityName;
-    torus.Scene = scene;
-
-    var triangularPrism1 = game.Create3DPrimitive(PrimitiveModelType.TriangularPrism);
-    triangularPrism1.Transform.Position = new Vector3(-8.0f, 2, -3.0f);
-    triangularPrism1.Transform.Rotation = Quaternion.RotationY(75);
-    triangularPrism1.Name = EntityName;
-    triangularPrism1.Scene = scene;
-
-    var triangularPrism2 = game.Create3DPrimitive(PrimitiveModelType.TriangularPrism);
-    triangularPrism2.Transform.Position = new Vector3(-3.5f, 0.5f, 1);
-    triangularPrism2.Name = EntityName;
-    triangularPrism2.Scene = scene;
+    void AddDebugComponents(Entity entity)
+    {
+        entity.Add(new DebugRenderComponentScript());
+        entity.Add(new CollidableGizmoScript()
+        {
+            Color = new Color4(0.4f, 0.843f, 0, 0.9f),
+            Visible = false
+        });
+    }
 }
 
 static void ResetTheScene(Scene scene)
@@ -110,9 +93,7 @@ static void ResetTheScene(Scene scene)
     var entities = scene.Entities.Where(e => e.Name == EntityName).ToList();
 
     foreach (var entity in entities)
-    {
         entity.Scene = null;
-    }
 }
 
 void DisplayInstructions() => instructions?.Print();
@@ -126,7 +107,8 @@ void InitializeDebugTextPrinter()
         DebugTextSystem = game.DebugTextSystem,
         TextSize = new(205, 17 * 4),
         ScreenSize = screenSize,
-        Instructions = [
+        Instructions =
+        [
             new("INSTRUCTIONS"),
             new("Press P to see collidables"),
             new("Press F11 to see debug meshes"),
